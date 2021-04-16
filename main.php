00a14c07bb8e69dbf505521ba9d025325d2bce37
K4N <andrushkakychkin@gmail.com>
2021-04-17 04:34:02 +0900

    main page maded 2/2 front-end

--- main.php
+++ main.php
@@ -19,26 +19,147 @@
 
     <!--Content-->
     <div class="col">
-       <!--Программа-->
+        <!--Программа-->
         <div class="row d-flex" style="height: 100vh;margin-top: 30px">
-           <div class="col-11 text-white mx-auto">
-               <div class="row">
-                   <div class="col">
-                       <div class="row my-2"><h2 class="ml-4">Как накачать грудные мышцы</h2></div>
-                       <div class="row my-2"><p class="ml-3">Учёные выяснили, что эти упражнения нагружают грудь по полной.</p></div>
-                       <div class="row my-2"><iframe class="mx-5" width="560" height="315" src="https://www.youtube.com/embed/q5q0_rHHOdA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                       <iframe class="mx-3" width="560" height="315" src="https://www.youtube.com/embed/q5q0_rHHOdA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
-                       <div class="row my-4"><p>Чтобы грудь стала рельефной, надо качать большие грудные мышцы. Для начала разберёмся, как они работают, а потом покажем упражнения.</p></div>
-                   </div>
-               </div>
-               <div class="row">
-                   <div class="col">
-                       <div class="row my-2"><h2 class="ml-4">Как накачать грудные мышцы</h2></div>
-                       <div class="row my-2"><p class="ml-3">Представьте, что грудная мышца — это резинка, которая соединяет плечо, ключицу и грудину. Если поднять руку в сторону, а потом потянуть за резинку, она переведёт плечо вперёд. Вы постоянно совершаете это движение в обычной жизни — например, когда закрываете дверь, с силой отталкиваете что-то или бьёте локтём в челюсть.</p></div>
-                       <div class="row my-4"><p class="ml-3">Большинство упражнений на грудные мышцы повторяют это движение. Разница лишь в том, что в них также используется штанга, гантели или стропы кроссовера для создания дополнительного сопротивления.</p></div>
-                   </div>
-               </div>
-           </div>
+            <div class="col-11 text-white mx-auto">
+                <div class="row">
+                    <div class="col">
+                        <div class="row my-2">
+                            <h2 class="ml-4">Как накачать грудные мышцы</h2>
+                        </div>
+                        <div class="row my-2">
+                            <p class="ml-3">Учёные выяснили, что эти упражнения нагружают грудь по полной.</p>
+                        </div>
+                        <div class="row my-2"><iframe class="mx-5" width="560" height="315" src="https://www.youtube.com/embed/q5q0_rHHOdA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            <iframe class="mx-3" width="560" height="315" src="https://www.youtube.com/embed/q5q0_rHHOdA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+                        <div class="row my-4">
+                            <p>Чтобы грудь стала рельефной, надо качать большие грудные мышцы. Для начала разберёмся, как они работают, а потом покажем упражнения.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="row my-2">
+                            <h2 class="ml-4">Как накачать грудные мышцы</h2>
+                        </div>
+                        <div class="row my-2">
+                            <p class="ml-3">Представьте, что грудная мышца — это резинка, которая соединяет плечо, ключицу и грудину. Если поднять руку в сторону, а потом потянуть за резинку, она переведёт плечо вперёд. Вы постоянно совершаете это движение в обычной жизни — например, когда закрываете дверь, с силой отталкиваете что-то или бьёте локтём в челюсть.</p>
+                        </div>
+                        <div class="row my-4">
+                            <p class="ml-3">Большинство упражнений на грудные мышцы повторяют это движение. Разница лишь в том, что в них также используется штанга, гантели или стропы кроссовера для создания дополнительного сопротивления.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row d-flex" style="height: 200vh;margin-top: 60px">
+            <div class="col-10 text-white mx-auto">
+                <div class="row d-flex">
+                    <h2 class="mx-auto">Гантели</h2>
+                </div>
+                <!--Каталог-->
+                <div class="row bg-white catalog">
+                    <div class="col">
+                        <div class="row mt-3">
+                            <div class="col-3 mx-auto bg-tovar">
+                                <div class="row tovar shadow-lg" style="height:250px;">
+                                    <div class="price">
+                                        <p class="priceNumber">8400 RUB</p>
+                                    </div>
+                                </div>
+                                <div class="row bg-white tovar shadow-lg mt-3">
+                                    <p class="name mx-auto">B.O.M.J Black</p>
+                                </div>
+                            </div>
+                            <div class="col-3 mx-auto bg-tovar">
+                                <div class="row tovar shadow-lg" style="height:250px;">
+                                    <div class="price">
+                                        <p class="priceNumber">8400 RUB</p>
+                                    </div>
+                                </div>
+                                <div class="row bg-white tovar shadow-lg mt-3">
+                                    <p class="name mx-auto">B.O.M.J Black</p>
+                                </div>
+                            </div>
+                            <div class="col-3 mx-auto bg-tovar">
+                                <div class="row tovar shadow-lg" style="height:250px;">
+                                    <div class="price">
+                                        <p class="priceNumber">8400 RUB</p>
+                                    </div>
+                                </div>
+                                <div class="row bg-white tovar shadow-lg mt-3">
+                                    <p class="name mx-auto">B.O.M.J Black</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row"  style="margin-top:100px;">
+                            <div class="col-3 mx-auto bg-tovar">
+                                <div class="row tovar shadow-lg" style="height:250px;">
+                                    <div class="price">
+                                        <p class="priceNumber">8400 RUB</p>
+                                    </div>
+                                </div>
+                                <div class="row bg-white tovar shadow-lg mt-3">
+                                    <p class="name mx-auto">B.O.M.J Black</p>
+                                </div>
+                            </div>
+                            <div class="col-3 mx-auto bg-tovar">
+                                <div class="row tovar shadow-lg" style="height:250px;">
+                                    <div class="price">
+                                        <p class="priceNumber">8400 RUB</p>
+                                    </div>
+                                </div>
+                                <div class="row bg-white tovar shadow-lg mt-3">
+                                    <p class="name mx-auto">B.O.M.J Black</p>
+                                </div>
+                            </div>
+                            <div class="col-3 mx-auto bg-tovar">
+                                <div class="row tovar shadow-lg" style="height:250px;">
+                                    <div class="price">
+                                        <p class="priceNumber">8400 RUB</p>
+                                    </div>
+                                </div>
+                                <div class="row bg-white tovar shadow-lg mt-3">
+                                    <p class="name mx-auto">B.O.M.J Black</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row" style="margin-top:100px;">
+                            <div class="col-3 mx-auto bg-tovar">
+                                <div class="row tovar shadow-lg" style="height:250px;">
+                                    <div class="price">
+                                        <p class="priceNumber">8400 RUB</p>
+                                    </div>
+                                </div>
+                                <div class="row bg-white tovar shadow-lg mt-3">
+                                    <p class="name mx-auto">B.O.M.J Black</p>
+                                </div>
+                            </div>
+                            <div class="col-3 mx-auto bg-tovar">
+                                <div class="row tovar shadow-lg" style="height:250px;">
+                                    <div class="price">
+                                        <p class="priceNumber">8400 RUB</p>
+                                    </div>
+                                </div>
+                                <div class="row bg-white tovar shadow-lg mt-3">
+                                    <p class="name mx-auto">B.O.M.J Black</p>
+                                </div>
+                            </div>
+                            <div class="col-3 mx-auto bg-tovar">
+                                <div class="row tovar shadow-lg" style="height:250px;">
+                                    <div class="price">
+                                        <p class="priceNumber">8400 RUB</p>
+                                    </div>
+                                </div>
+                                <div class="row bg-white tovar shadow-lg mt-3">
+                                    <p class="name mx-auto">B.O.M.J Black</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
